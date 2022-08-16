dc127ac40a72a65dc8322185b5257e2ca645a094
CodeiSanD <codeisand@gmail.com>
2022-08-17 00:31:30 +0530

    readme 1st

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'code.isand.git',
+  appName: 'git_Test',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
